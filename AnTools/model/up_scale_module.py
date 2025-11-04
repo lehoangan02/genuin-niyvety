f9eec85d86282871c98bc636d574f1f25dfd1ff2
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-11-04 20:50:48 +0700

    fix upscale module

--- AnTools/model/up_scale_module.py
+++ AnTools/model/up_scale_module.py
@@ -6,19 +6,19 @@ class NoPromptUpScaleModule(nn.Module):
     def __init__(self, c_low, c_high, c_out):
         super().__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv2d(c_high, c_out, kernel_size=3, padding=1, stride=1),
-            nn.BatchNorm2d(c_out),
+            nn.Conv2d(c_low, c_high, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(c_high),
             nn.LeakyReLU(0.01, inplace=True)
         )
         self.conv2concat = nn.Sequential(
-            nn.Conv2d(c_out + c_low, c_out, kernel_size=3, padding=1, stride=1),
+            nn.Conv2d(c_high + c_high, c_out, kernel_size=3, padding=1, stride=1),
             nn.BatchNorm2d(c_out),
             nn.LeakyReLU(0.01, inplace=True)
         )
 
     def forward(self, low_res, high_res):
         low_res_up = F.interpolate(low_res, size=high_res.shape[-2:], mode='bilinear', align_corners=True)
-        high_res = self.conv1(high_res)
+        low_res_up = self.conv1(low_res_up)
         x = torch.cat((high_res, low_res_up), dim=1)
         x = self.conv2concat(x)
         return x
